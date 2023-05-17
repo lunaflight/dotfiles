66faf7e81f362bba2ad8d39865f9481dbd078e7f
et-irl <e0958283@u.nus.edu>
2023-05-17 22:35:45 +0800

    Update

--- dot_config/nvim/lua/core/plugin_config/lsp_config.lua
+++ dot_config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
   -- https://github.com/williamboman/mason-lspconfig.nvim
-  ensure_installed = { "lua_ls", "jdtls" }
+  ensure_installed = { "clangd", "lua_ls", "jdtls" }
 })
 
 local on_attach = function(_, _)
@@ -18,3 +18,4 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").lua_ls.setup { on_attach = on_attach, capabilities = capabilities }
 require("lspconfig").jdtls.setup { on_attach = on_attach, capabilities = capabilities }
+require("lspconfig").clangd.setup { on_attach = on_attach, capabilities = capabilities }
